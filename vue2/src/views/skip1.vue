d2ab337c1b65ea797059be5fa520f6abe7d75d62
chenjiawei@zhuojianchina.com <chenjiawei@zhuojianchina.com>
2024-02-20 11:15:34 +0800

    mixin2

--- vue2/src/views/skip1.vue
+++ vue2/src/views/skip1.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="skip1">
-        <h1>skip1</h1>
+        <h1>{{ msg }}</h1>
         <el-button @click="skip('chenjiawei')">skip</el-button>
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
   
 <script>
-import skip from '@/mixins/skip.js'  
+import skip from '@/mixins/skip.js'
+import deptList from '@/mixins/deptList.js'
 export default {
     name: 'skip1',
-    mixins: [skip],
+    mixins: [skip, deptList],
 }
 </script>
